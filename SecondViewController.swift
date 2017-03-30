badab2850d0ed1780e2044b6aff62f25e97dfc13
William Castellano <wcastellano13@gmail.com>
2017-03-30 14:52:43 -0400

    Added Segue to Send users selections to 2nd View controller, Added Picker for Tournamnets Available

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -10,17 +10,54 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-  @IBOutlet var pairingsLabel: UILabel!
-  var year = "25"
+  var year = ""
+  var tournamentID = ""
+  var round = ""
   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+  
+      // Replaces Tournament Names with The IDs for URL
+      if tournamentID == "Valspar" {
+        tournamentID = "df42e950-c6a7-4b3d-8b93-41efa1bfdf82"
+      } else if tournamentID == "Masters" {
+        tournamentID = "c0f3aba7-ead2-41bb-8b84-0dbf39370099"
+      } else if tournamentID == "Mexico" {
+        tournamentID = "deb641b0-3480-42d0-bbcb-46f19d469fc3"
+      } else {
+        tournamentID = "efa623e0-36a9-4644-b5a1-c1276c8f04b5"
+      }
       
-      print(year)
+
+      let url = URL(string: "https://api.sportradar.us/golf-t2/teetimes/pga/2017/tournaments/" + tournamentID + "/Rounds/" + round + "/teetimes.json?api_key=u4thm6q3payb9kw6sp8yq3sm")!
       
-    }
+      let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        if error != nil {
+          print(error)
+        } else {
+          
+          if let urlContent = data {
+            
+            do {
+              let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+              
+//              print(jsonResult)
+              
+              if let name = jsonResult["name"] as? String {
+                print(name)
+              }
+              
+            } catch {
+            // process errors
+            }
+          }
+        }
+      }
+      task.resume()
+
+  }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- pairingsGuide/ViewController.swift
+++ pairingsGuide/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-  @IBOutlet var yaerTextfield: UITextField!
+  
+  @IBOutlet var yearTextfield: UITextField!
   @IBOutlet var tournamentTextfield: UITextField!
   @IBOutlet var roundTextfield: UITextField!
+  
+  
+  let yearPickerData = ["2017", "2016", "2015"]
+  let tournamentPickerData = ["Valspar", "Mexico", "Masters", "Shell Houston Open"]
+  let roundPickerData = ["1", "2", "3", "4"]
   
   @IBAction func submit(_ sender: Any) {
   }
@@ -20,8 +26,63 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+     let picker = UIPickerView()
+     tournamentTextfield.inputView = picker
+     picker.delegate = self
+    
   }
-
+  
+  // PICKER FUNCTIONS
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    
+    return tournamentPickerData.count
+    
+    
+//    if (yearTextfield.tag == 1) {
+//      return yearPickerData.count
+//    } else if (tournamentTextfield.tag) == 2 {
+//      return tournamentPickerData.count
+//    } else {
+//      return roundPickerData.count
+//    }
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    
+    return tournamentPickerData[row]
+    
+//    if (yearTextfield.tag == 1) {
+//      return yearPickerData[row]
+//    } else if (tournamentTextfield.tag == 2) {
+//      return tournamentPickerData[row]
+//    } else {
+//      return roundPickerData[row]
+//    }
+    
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    
+    tournamentTextfield.text = tournamentPickerData[row]
+    
+//    if (yearTextfield.tag == 1) {
+//      yearTextfield.text = yearPickerData[row]
+//    } else if (tournamentTextfield.tag) == 2 {
+//      tournamentTextfield.text = tournamentPickerData[row]
+//    } else {
+//      roundTextfield.text = roundPickerData[row]
+//    }
+    
+  }
+  
+  
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
@@ -31,7 +92,10 @@ class ViewController: UIViewController {
     if segue.identifier == "showSecondViewController" {
       let secondViewController = segue.destination as! SecondViewController
       
-      secondViewController.year = yaerTextfield.text!
+      secondViewController.year = yearTextfield.text!
+      secondViewController.round = roundTextfield.text!
+      secondViewController.tournamentID = tournamentTextfield.text!
+      
     }
   }
 
